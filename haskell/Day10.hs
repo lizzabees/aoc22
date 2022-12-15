b4c6b3484b41d675b2d75e68d470c2c66169f41e
Elizabeth Ryan <bees@minerva.local>
2022-12-15 03:24:11 -0800

    done with day 10hs

--- haskell/Day10.hs
+++ haskell/Day10.hs
@@ -1,38 +1,101 @@
 #!/usr/bin/env cabal
 {- cabal:
-build-depends: base, mtl, optics, parsec
+build-depends: base, lens, mtl, parsec, vector
 ghc-options: -O2
 -}
+{-# LANGUAGE NamedFieldPuns  #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TemplateHaskell #-}
 module Main where
-import Control.Lens
-import Control.Lens.TH
-import Data.Functor ((<&>), ($>))
+import Control.Monad.State
+import Data.Functor ((<&>),($>))
+import Data.Vector ((!))
 import System.Environment (getArgs)
 import Text.Parsec hiding (State,parse)
 
--- CPU stuff
-data Inst = Noop
+import qualified Control.Monad.ST as ST
+import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector as V
+
+-- CPU
+data Insn = Noop
           | Addx Int
-          deriving (Eq,Ord,Show)
+          deriving(Eq,Ord,Show)
 
-
-cpuc :: Inst -> Int
-cpuc (Noop  ) = 1
-cpuc (Addx _) = 2
+cycles :: Insn -> Int
+cycles (Noop  ) = 1
+cycles (Addx _) = 2
 
 data Cpu = Cpu
-    { _regX :: Int
-    , _pipe :: [Inst]
+    { _cpuX     :: Int
+    , _cpuCycle :: Int
+    , _cpuIsns  :: [(Int, Insn)]
     } deriving(Eq,Ord,Show)
 
-$(makeLenses ''Cpu)
+cpu :: [Insn] -> Cpu
+cpu isns = Cpu 1 0 $ zip (map cycles isns) isns
 
--- step one instruction thru the cpu
-step :: Inst -> Cpu -> Cpu
-step (Noop  ) = id
-step (Addx n) = regX %~ (+n) 
+-- exec one insn
+exec :: Cpu -> Insn -> Cpu
+exec c            (Noop  ) = c
+exec s@Cpu{_cpuX} (Addx n) = s { _cpuX = _cpuX + n }
+
+-- step through one cpu cycle
+step :: Cpu -> Cpu
+step c@Cpu{..} =
+    let cycle' = _cpuCycle + 1
+     in case _cpuIsns of
+          [        ] -> error "step on empty insn list"
+          ((1,i):is) -> (exec c i) {_cpuCycle = cycle', _cpuIsns = is}
+          ((n,i):is) ->  c         {_cpuCycle = cycle', _cpuIsns = (n-1,i):is}
+
+-- take n steps through the code
+stepN :: Cpu -> Int -> Cpu
+stepN c 1 = c
+stepN c n = stepN (step c) (n-1)
+
+-- get signal strength
+signal :: Cpu -> Int
+signal Cpu{..} = (_cpuCycle + 1) * _cpuX
+
+-- sample signal strength *on* the listed cycles
+sample :: Cpu -> [Int] -> [Int]
+sample c = fst . flip runState c . flip go []
+    where go :: [Int] -> [Int] -> State Cpu [Int]
+          go [    ] acc = return . reverse $ acc
+          go (x:xs) acc = do
+              Cpu{..} <- get
+              let n  = x - _cpuCycle
+              let c' = stepN c n
+              let s  = signal c'
+              put c' 
+              go xs (s:acc)
+
+-- for a given crt x is the sprit visible
+contains :: Cpu -> Int -> Bool
+contains Cpu{_cpuX} x = abs (_cpuX - x) <= 1
+
+runCrt :: Cpu -> V.Vector Bool
+runCrt c = ST.runST $ do
+    pixels <- MV.replicate (40*6) False
+    let loop x y c@Cpu{..} =
+            if null $ _cpuIsns
+               then V.freeze pixels
+               else do
+                   when (c `contains` x) $ MV.write pixels (y*40+x) True
+                   let c' = step c
+                   let x' = (x + 1) `mod` 40
+                   let y' = if x' == 0 then y+1 else y
+                   loop x' y' c'
+    loop 0 0 c 
+
+printCrt :: V.Vector Bool -> IO ()
+printCrt v = go 0
+    where sz   = V.length v
+          go i = do
+            let i' = i +1
+            if v ! i then putChar '#' else putChar '.'
+            when ((i' `mod` 40) == 0) $ putChar '\n'
+            if i' == sz then return () else go i'
 
 -- PARSING
 type Parser a = Parsec String () a
@@ -52,21 +115,25 @@ pint = do
       Just  s -> pure $ read (s:num)
       Nothing -> pure $ read    num
 
-inst :: Parser Inst
-inst = noop <|> addx
+insn :: Parser Insn
+insn = noop <|> addx
     where noop = string "noop"  $> Noop
           addx = string "addx " *> pint <&> Addx
 
-prog :: Parser [Inst]
-prog = manyTill (inst <* newline) eof
+prog :: Parser [Insn]
+prog = manyTill (insn <* newline) eof
 
 -- 
-part1 :: Int
-part1 = undefined
+part1 :: [Insn] -> Int
+part1 insns = sum $ sample (cpu insns) [20,60..220]
 
+part2 :: [Insn] -> IO ()
+part2 = printCrt . runCrt . cpu
 
 main :: IO ()
 main = do
     path  <- head <$> getArgs
     input <- parseFile path prog
-    putStrLn $ mconcat ["part 1: ", show input]
+    putStrLn $ mconcat ["part 1: ", show $ part1 input]
+    putStrLn $ mconcat ["part 2: --------------------"]
+    part2 input

f346f923e04aff410b5d3dba9d5d2f9729d980b6
Elizabeth Ryan <bees@minerva.lan>
2022-12-25 16:16:04 -1000

    placeholder commit, refactoring prelude

--- haskell/Day12.hs
+++ haskell/Day12.hs
@@ -3,20 +3,23 @@
 build-depends: base, pqueue
 ghc-options: -O2
 -}
+{-# LANGUAGE InstanceSigs    #-}
 {-# LANGUAGE NamedFieldPuns  #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 import Control.Monad (guard,void)
-import Control.Monad.ST
+import Data.Bifunctor (bimap)
+import Data.Function (on)
 import Data.Char (ord)
 import Data.Vector ((!))
 import Lens.Micro.Platform
 import System.Environment (getArgs)
 import Text.Parsec hiding (parse)
 
+import qualified Data.Map.Strict as M
 import qualified Data.PQueue.Min as PQ
-import qualified Data.Vector as V
+import qualified Data.Vector     as V
 
 data HeightInput = Height Int
                  | Start
@@ -29,7 +32,6 @@ data HeightMap = HeightMap
     , _hmWidth  :: Int
     , _hmHeight :: Int
     } deriving(Eq,Ord,Show)
-
 makeLenses ''HeightMap
 
 type Point = (Int,Int)
@@ -47,27 +49,23 @@ valAt m = valIdx m . idxAt m
 canMove :: Int -> Int -> Bool
 canMove p p' = p' <= (p + 1)
 
--- manhattan distance
-mdist :: Point -> Point -> Int
-mdist (px,py) (dx,dy) = abs (dx-px) + abs (dy-py)
-
 -- PATHFINDING
 data PathFind = PathFind
     { _pfHeights :: HeightMap
     , _pfStart   :: Point
     , _pfEnd     :: Point
     } deriving(Eq,Ord,Show)
-
 makeLenses ''PathFind
 
 -- returns a list of possible next moves for a
--- given point, filtering out moves that are off
+-- given point, filtering out mkoves that are off
 -- the map or un-passable
 posMoves :: HeightMap -> Point -> [Point]
-posMoves m@HeightMap{..} p@(x,y) =
-    let pIdx = idxAt  m p
-        pVal = valIdx m pIdx
-        nbrs = [(x,y-1), (x+1,y), (x,y+1), (x-1,y)]
+posMoves m@HeightMap{..} p =
+    let pIdx  = idxAt  m p
+        pVal  = valIdx m pIdx
+        arnd  = [((+x),(+y)) | y <- [-1,1], x <- [-1,1]]
+        nbrs = zipWith (uncurry bimap) arnd $ repeat p
      in do
          n@(nx,ny) <- nbrs
          guard $  nx >= 0 && nx < _hmWidth && ny >= 0 && ny < _hmHeight
@@ -76,12 +74,36 @@ posMoves m@HeightMap{..} p@(x,y) =
          guard $ vdel <= 1
          return n
 
--- heuristic function
-type Hfn = HeightMap -> Point -> Int
+type CostFn = HeightMap -> Point -> Point -> Int
+type NextPQ = PQ.MinQueue (Int,  Point )
+type VisitM = M.Map Point Node
 
-astar :: PathFind -> Hfn -> Maybe [Point]
-astar = undefined
-            
+-- manhattan distance
+mdist :: CostFn
+mdist _ (px,py) (dx,dy) = abs (dx-px) + abs (dy-py)
+
+data Node = Node
+    { _noPath :: [Point] -- path back to origin
+    , _noCost :: Int     -- cost so far to here
+    } deriving (Eq,Show)
+makeLenses ''Node
+
+instance Ord Node where
+    compare :: Node -> Node -> Ordering
+    compare = compare `on` _noCost
+
+-- in a* terms, we take the map, g(f) which is a function
+-- calculating cost to move from one point to another and
+-- h(f) which is a heuristic function guessing cost between
+-- two points
+astar :: PathFind -> CostFn -> CostFn -> Maybe [Point]
+astar PathFind{..} gf hf =
+    let cost    = hf _pfHeights _pfStart _pfEnd
+        next    = PQ.singleton (cost, _pfStart)
+        visited = M.empty
+     in go next visited
+    where go = undefined
+
 -- PARSING
 data ParseState = ParseState 
     { _psX      :: Int

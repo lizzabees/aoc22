739b3bf3397625991ec060106a5a3e78cdf84cbe
Elizabeth Ryan <bees@minerva.lan>
2022-12-26 02:57:15 -1000

    day12 solved though part2 makes me want to re-implment for many-origins

--- haskell/Day12.hs
+++ haskell/Day12.hs
@@ -3,157 +3,157 @@
 build-depends: base, pqueue
 ghc-options: -O2
 -}
-{-# LANGUAGE InstanceSigs    #-}
 {-# LANGUAGE NamedFieldPuns  #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
+import Prelude hiding (try)
 import Data.Vector ((!))
 import Lens.Micro.Platform
 import Text.Parsec hiding (parse)
 
 import qualified Data.Map.Strict as M
-import qualified Data.PQueue.Min as PQ
-import qualified Data.Vector     as V
+import qualified Data.PQueue.Prio.Min as PQ
 
-data HeightInput = Height Int
-                 | Start
-                 | Target
-                 deriving(Eq,Ord,Show)
-
--- HEIGHTMAP 
-data HeightMap = HeightMap 
-    { _hmData   :: V.Vector Int
-    , _hmWidth  :: Int
-    , _hmHeight :: Int
-    } deriving(Eq,Ord,Show)
-makeLenses ''HeightMap
-
+-- HEIGHTMAPS
 type Point = (Int,Int)
 
--- this is partial for invalid inputs. whocare
-valAt :: HeightMap -> Point -> Int
-valAt HeightMap{_hmData,_hmWidth} (x,y) = _hmData ! y * _hmWidth + x
+data HeightMap = HeightMap
+    { _width  :: Int
+    , _height :: Int
+    , _hdata  :: Vector Int
+    } deriving(Show)
+makeClassy ''HeightMap
 
-canMove :: Int -> Int -> Bool
-canMove p p' = p' <= (p + 1)
+hmGet :: HeightMap -> Point -> Int
+hmGet HeightMap{_width,_hdata} (x,y) = _hdata ! (y * _width + x)
+
+data Input = Input
+    { _start :: Point
+    , _stop  :: Point
+    , _hmap   :: HeightMap
+    } deriving(Show)
+makeClassy ''Input
+
+instance HasHeightMap Input where
+    heightMap = hmap
 
 -- PATHFINDING
-data PathFind = PathFind
-    { _pfHeights :: HeightMap
-    , _pfStart   :: Point
-    , _pfEnd     :: Point
-    } deriving(Eq,Ord,Show)
-makeLenses ''PathFind
+buildPath :: Point -> Map Point Point -> [Point]
+buildPath p paths = go p []
+    where go :: Point -> [Point] -> [Point]
+          go p acc = case paths ^? (ix p) of
+                       Nothing -> reverse acc
+                       Just p' -> go p' (p:acc)
 
-data Node = Node
-    { _noPos  :: Point
-    , _noPath :: [Point] -- path back to origin
-    , _noCost :: Int     -- cost so far 
-    , _noDist :: Int     -- heuristic to dest
-    } deriving (Eq,Show)
-makeLenses ''Node
-
-instance Ord Node where
-    compare :: Node -> Node -> Ordering
-    compare = compare `on` _noCost
-
--- returns a list of possible next moves for a
--- given point, filtering out mkoves that are off
--- the map or un-passable
-neighbors :: PathFind -> Point -> [Point]
-neighbors pf p =
-    let pval = valAt (pf^.pfHeights) (pf^.pfStart)
-        arnd = [((+x),(+y)) | y <- [-1,1], x <- [-1,1]]
-        nbrs = zipWith (uncurry bimap) arnd $ repeat p
-        width  = pf^.pfHeights.hmWidth
-        height = pf^.pfHeights.hmHeight
+neighbors :: HeightMap -> Point -> [Point]
+neighbors hm p =
+    let dirs    = map (bimap (+) (+)) $ [(0,-1), (1,0), (0,1), (-1,0)]
+        nbrs    = zipWith (uncurry bimap) dirs $ repeat p
+        width'  = hm^.width
+        height' = hm^.height
+        currh   = hm `hmGet` p
      in do
          n@(nx,ny) <- nbrs
-         guard $ nx >= 0 && nx < width && ny >= 0 && ny < height
-         let nval = valAt (pf^.pfHeights) n
-         let vdel = nval-pval
-         guard $ vdel <= 1
+         guard $ nx >= 0 && nx < width'
+         guard $ ny >= 0 && ny < height'
+         let nbrh = hm `hmGet` n 
+         guard $ nbrh - currh <= 1
          return n
 
-type CostFn = PathFind -> Point -> Point -> Int
-
-type Front = PQ.MinQueue Node
-type Visit = M.Map Point Node
-
--- a* state
 data AState = AState
-    { _asFront :: Front
-    , _asVisit :: Visit
+    { _front :: PQ.MinPQueue Int Point
+    , _costs :: Map Point Int
+    , _paths :: Map Point Point
     } deriving(Show)
 makeLenses ''AState
 
--- manhattan distance
-mdist :: CostFn
-mdist _ (px,py) (dx,dy) = abs (dx-px) + abs (dy-py)
+data Node = Node
+    { _pos  :: Point
+    , _cost :: Int
+    , _dist :: Int
+    } deriving(Show)
+makeLenses ''Node
 
--- in a* terms, we take the map, g(f) which is a function
--- calculating cost to move from one point to another and
--- h(f) which is a heuristic function guessing cost between
--- two points
-astar :: PathFind -> CostFn -> CostFn -> Maybe [Point]
-astar pf@PathFind{..} gf hf =
-    let dist  = hf pf _pfStart _pfEnd
-        node  = Node _pfStart [] 0 dist
-        front = PQ.singleton node
-        visit = M.singleton _pfStart node
-        state = AState front visit
-     in step state $ Just node
-    where step :: AState -> Maybe Node -> Maybe [Point]
-          step _     Nothing     = Nothing -- we didn't find target
-          step _     (Just curr) | (curr^.noPos) == (pf^.pfEnd) = Just $ path curr
-          step state (Just curr) =
-              let nbrs   = neighbors pf (curr^.noPos)
-                  ncosts = flip map nbrs $ gf pf (curr^.noPos)
-                  ndists = flip map nbrs $ flip (hf pf) (pf^.pfEnd)
-                  npaths = repeat $ path curr
-                  nbrs'  = Node <$> nbrs <*> npaths <*> ncosts <*> ndists
-                  state' = flip execState state $ do
-                      modify . over asFront $ PQ.deleteMin
-                      forM_ nbrs' $ \nbr -> undefined
-               in step state' $ PQ.getMin (state'^.asFront)
-          path :: Node -> [Point]
-          path Node{_noPos,_noPath} = _noPos:_noPath
+type DistFn = Point -> Point -> Int
+
+-- here thar be dragons
+aStar :: Input -> DistFn -> DistFn -> Maybe [Point]
+aStar input gf hf =
+    let begin  = input^.start
+        front' = PQ.singleton 0 begin
+        costs' = M.singleton begin 0
+        paths' = M.empty
+        state  = AState front' costs' paths'
+     in go state $ Just begin
+    where go :: AState -> Maybe Point -> Maybe [Point]
+          go _     Nothing  = Nothing
+          go state (Just p) | p == (input^.stop) = Just $ buildPath p (state^.paths)
+          go state (Just p) =
+              let front' = PQ.deleteMin (state^.front)
+                  nposes = neighbors (input^.hmap) p
+                  ncosts = flip map nposes $ gf p
+                  ndists = flip map nposes $ hf (input^.stop)
+                  nbrs   = Node <$> nposes <*> ncosts <*> ndists
+                  state' = step (state & front .~ front') p nbrs
+                  _      = unsafePerformIO $ print state'
+               in go state' . fmap snd $ PQ.getMin (state'^.front)
+          step :: AState -> Point -> [Node] -> AState
+          step state _    [    ] = state
+          step state from (n:ns) =
+              let ccost = state ^. costs ^?! ix from
+                  ncost = ccost + gf from (n^.pos)
+               in case state ^. costs ^? ix (n^.pos) of
+                      Nothing                    -> step (addNew state from ncost n) from ns
+                      Just ocost | ncost < ocost -> step (addNew state from ncost n) from ns
+                      Just _                     -> step state from ns
+          addNew :: AState -> Point -> Int -> Node -> AState
+          addNew state from cost new =
+              let npri = cost + hf (new^.pos) (input^.stop)
+                  npos = new^.pos
+               in state
+                & costs %~ M.insert npos cost
+                & front %~ PQ.insert npri (new^.pos)
+                & paths %~ M.insert npos from
+
+manhattan :: DistFn
+manhattan (lx,ly) (rx,ry) = abs (lx - rx) + abs (ly - ry)
+
+const2 :: a -> b -> c -> a
+const2 a _ _ = a
 
 -- PARSING
-data ParseState = ParseState 
-    { _psX      :: Int
+data PState = PState
+    { _psStart  :: Point
+    , _psStop   :: Point
+    , _psX      :: Int
     , _psY      :: Int
     , _psWidth  :: Int
     , _psHeight :: Int
-    , _psStart  :: Point
-    , _psEnd    :: Point
-    } deriving (Eq,Ord,Show)
-makeLenses ''ParseState
+    } deriving(Show)
+makeLenses ''PState
 
-initState :: ParseState
-initState = ParseState 0 0 0 0 (0,0) (0,0)
-
-type Parser a = Parsec String ParseState a
+type Parser a = Parsec String PState a
 
 parse :: Parser a -> String -> String -> a
-parse p n = either (error . show) id . runParser p initState n
+parse p f = either (error . show) id . runParser p init' f
+    where init' = PState (0,0) (0,0) 0 0 0 0
 
-parseFile :: String -> Parser a -> IO a
-parseFile n p = parse p n <$> readFile n
+parseFile :: Parser a -> FilePath -> IO a
+parseFile p f = parse p f <$> readFile f
 
-heightMap :: Parser PathFind
-heightMap = do
-    heights' <- V.fromList . concat <$> heights
-    ParseState{..} <- getState
-    pure $ PathFind (HeightMap heights' _psWidth _psHeight) _psStart _psEnd
+pInput :: Parser Input
+pInput = do
+    hdata' <- fromList . concat <$> heights
+    PState{..} <- getState
+    pure $ Input _psStart _psStop (HeightMap _psWidth _psHeight hdata')
         where heights :: Parser [[Int]]
               heights = manyTill line eof'
-              line    :: Parser [Int]
-              line    = many1 item <* newline' 
-              item    :: Parser Int
-              item    = choice [height, start, end]
-              height  :: Parser Int
+              line :: Parser [Int]
+              line = many1 item <* newline'
+              item :: Parser Int
+              item = choice [height, start, stop]
+              height :: Parser Int
               height = do
                   c <- oneOf ['a'..'z']
                   let a = ord 'a'
@@ -164,32 +164,45 @@ heightMap = do
               start = do
                   void $ char 'S'
                   modifyState $ \s -> s
-                    & psStart .~ (s ^. psX, s ^. psY)
+                    & psStart .~ (s^.psX, s^.psY)
                     & psX +~ 1
                   pure 0
-              end :: Parser Int
-              end = do
+              stop = do
                   void $ char 'E'
                   modifyState $ \s -> s
-                    & psEnd .~ (s ^. psX, s ^. psY)
+                    & psStop .~ (s^.psX,s^.psY)
                     & psX +~ 1
                   pure 25
               newline' :: Parser ()
               newline' = do
                   void newline
                   modifyState $ \s -> s
-                    & psWidth .~ s ^. psX
+                    & psWidth .~ s^.psX
                     & psX .~ 0
                     & psY +~ 1
-                  return ()
+                  pure ()
               eof' :: Parser ()
               eof' = do
-                  eof
+                  void eof
                   modifyState $ \s -> s
-                    & psHeight .~ s ^. psY
+                    & psHeight .~ s^.psY
+
+
+-- MAIN
+part1 :: Input -> Int
+part1 input = length . fromJust $ aStar input (const2 1) manhattan
+
+part2 :: Input -> Int
+part2 input =
+    let coords   = [(x,y) | y <- [0..input^.height-1], x <- [0..input^.width-1]]
+        cvals    = zip coords $ toList (input^.hmap^.hdata)
+        starts   = map fst . filter ((==0) . snd) $ cvals
+        aStar' p = aStar input{_start=p} (const2 1) manhattan
+     in head . sort . map (length . fromJust) . filter isJust . map aStar' $ starts
 
 main :: IO ()
 main = do
-    path <- head <$> getArgs
-    input <- parseFile path heightMap
-    print input
+    path  <- head <$> getArgs
+    input <- parseFile pInput path
+    putStrLn $ mconcat ["part1: ", show $ part1 input]
+    putStrLn $ mconcat ["part2: ", show $ part2 input]
